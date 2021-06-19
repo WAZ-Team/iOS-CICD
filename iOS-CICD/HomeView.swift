171c815badf3385dedf46319bda587dd052325c2
NamNH <unknown>
2021-06-19 23:45:32 +0700

    Add HomeView

---
+++ iOS-CICD/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  iOS-CICD
+//
+//  Created by Nguyễn Nam on 6/19/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
