644a7c174f6855c91f2ff7531dc5ed49865fc145
NamNH <unknown>
2021-06-20 15:23:56 +0700

    Test fastlane

--- fastlane/swift/Matchfile.swift
+++ fastlane/swift/Matchfile.swift
@@ -15,6 +15,14 @@ public class Matchfile: MatchfileProtocol {
     // If you want to enable `match`, run `fastlane match init`
     // After, this file will be replaced with a custom implementation that contains values you supplied
     // during the `init` process, and you won't see this message
+    var username: String {
+        return environmentVariable(get: "APPLE_ID")
+    }
+    
+    var teamId: String {
+        return environmentVariable(get: "TEAM_ID")
+    }
+    
     public var gitUrl: String {
         return "https://github.com/WAZ-Team/Certificates-Provisionings.git"
     }
@@ -24,25 +32,21 @@ public class Matchfile: MatchfileProtocol {
     }
     
     public var gitBasicAuthorization: String? {
-        return "namnh92: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"
+        return "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"
     }
     
-    public var gitPrivateKey: String? {
-        return """
-        -----BEGIN OPENSSH PRIVATE KEY-----
-        b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABBwi3ZlSH
-        FMjPhAaCEEBK5WAAAAEAAAAAEAAAAzAAAAC3NzaC1lZDI1NTE5AAAAICPoBW8R0LoxGGxY
-        bp4IyKs0PLrTVRD1LGz4F6FtnUVOAAAAoAkwee31ITFBv27nG0kdkHGtXdL6cZB8edg00u
-        QYvOXivNtE3Dzz1A06jFZJsN84Vgijz0g1vHKq5Znh9T4b9Ed5zO7KpG2Irwj+QoQheIrZ
-        RD4cc1Sx1Iojz/LLdUd7/YihK2s5j/xklfUQdBLbgYh7a5XAfR8wAIt+JmEz/FSLfnL13c
-        uPXgjC6ckmvq45dUSrTkdsa0F5PDzlin/xGHc=
-        -----END OPENSSH PRIVATE KEY-----
-        """
-    }
-    
-    public var keychainName: String {
-        return "app-signing.keychain-db"
-    }
+//    public var gitPrivateKey: String? {
+//        return """
+//        -----BEGIN OPENSSH PRIVATE KEY-----
+//        b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABBwi3ZlSH
+//        FMjPhAaCEEBK5WAAAAEAAAAAEAAAAzAAAAC3NzaC1lZDI1NTE5AAAAICPoBW8R0LoxGGxY
+//        bp4IyKs0PLrTVRD1LGz4F6FtnUVOAAAAoAkwee31ITFBv27nG0kdkHGtXdL6cZB8edg00u
+//        QYvOXivNtE3Dzz1A06jFZJsN84Vgijz0g1vHKq5Znh9T4b9Ed5zO7KpG2Irwj+QoQheIrZ
+//        RD4cc1Sx1Iojz/LLdUd7/YihK2s5j/xklfUQdBLbgYh7a5XAfR8wAIt+JmEz/FSLfnL13c
+//        uPXgjC6ckmvq45dUSrTkdsa0F5PDzlin/xGHc=
+//        -----END OPENSSH PRIVATE KEY-----
+//        """
+//    }
 }
 
 // Generated with fastlane 2.182.0
