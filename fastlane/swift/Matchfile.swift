5a2e7e2786fb7cc9d6f85fad87c8c353b26baf39
NamNH <unknown>
2021-06-20 19:05:29 +0700

    Fix fastlane

--- fastlane/swift/Matchfile.swift
+++ fastlane/swift/Matchfile.swift
@@ -16,11 +16,11 @@ public class Matchfile: MatchfileProtocol {
     // After, this file will be replaced with a custom implementation that contains values you supplied
     // during the `init` process, and you won't see this message
     public var username: String {
-        return "namnh@vmodev.com"
+        return environmentVariable(get: "APPLE_ID")
     }
     
     public var teamId: String {
-        return "H4C7478UQW"
+        return environmentVariable(get: "TEAM_ID")
     }
     
     public var gitUrl: String {
@@ -31,18 +31,7 @@ public class Matchfile: MatchfileProtocol {
         return "master"
     }
     
-    public var gitPrivateKey: String? {
-        return """
-        -----BEGIN OPENSSH PRIVATE KEY-----
-        b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABBwi3ZlSH
-        FMjPhAaCEEBK5WAAAAEAAAAAEAAAAzAAAAC3NzaC1lZDI1NTE5AAAAICPoBW8R0LoxGGxY
-        bp4IyKs0PLrTVRD1LGz4F6FtnUVOAAAAoAkwee31ITFBv27nG0kdkHGtXdL6cZB8edg00u
-        QYvOXivNtE3Dzz1A06jFZJsN84Vgijz0g1vHKq5Znh9T4b9Ed5zO7KpG2Irwj+QoQheIrZ
-        RD4cc1Sx1Iojz/LLdUd7/YihK2s5j/xklfUQdBLbgYh7a5XAfR8wAIt+JmEz/FSLfnL13c
-        uPXgjC6ckmvq45dUSrTkdsa0F5PDzlin/xGHc=
-        -----END OPENSSH PRIVATE KEY-----
-        """
-    }
+    
 }
 
 // Generated with fastlane 2.182.0
